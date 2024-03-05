de34b85ff812ad2843b22dc15b04d1a45c6e8a25
abulezz <ahmedrezz@gmail.com>
2024-03-05 11:15:10 +0100

    .env minor edits

--- server/controler/userController.js
+++ server/controler/userController.js
@@ -6,8 +6,8 @@ export const testRoute = (req, res) => {
 
 export const getAllUsers = async (req, res) => {
   try {
-    const users = await UserModel.find();
-    res.status(200).json(users);
+    const allUsers = await UserModel.find();
+    res.status(200).json(allUsers);
   } catch {
     res.status(404).json({ message: error.message });
   }
--- server/testRouter.js
+++ server/testRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { testFunction } from "./controler/testController.js";
+import { getAllUsers } from "./controler/userController.js";
 
 const TestRouter = express.Router();
 
 TestRouter.get("/getAll", testFunction);
+TestRouter.get("/allusers", getAllUsers);
 
 export default TestRouter;
