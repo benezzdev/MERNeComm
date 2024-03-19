4a0b55453c7564dde78b54e4b9dd61b3346ab8d0
abulezz <ahmedrezz@gmail.com>
2024-03-19 10:13:35 +0100

    userLogin corrected

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -3,6 +3,7 @@ import {
   createUser,
   getAllUsers,
   getOneUser,
+  loginUser,
 } from "../controler/userController.js";
 import { multerUpload } from "../middleware/multer.js";
 const UserRouter = express.Router();
@@ -10,6 +11,6 @@ const UserRouter = express.Router();
 UserRouter.get("/allusers", getAllUsers);
 UserRouter.get("/:id", getOneUser);
 UserRouter.post("/register", multerUpload.single("avatar"), createUser);
-UserRouter.post("/login, loginUser");
+UserRouter.post("/login", loginUser);
 
 export default UserRouter;
